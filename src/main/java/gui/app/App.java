65ccf2adeda6734535ebe16f63e9bbc1ffb3a4c9
Long <somethings.ttt@gmail.com>
2024-12-23 15:54:11 +0700

    eliminate year mode

--- src/main/java/gui/app/App.java
+++ src/main/java/gui/app/App.java
@@ -15,18 +15,15 @@ public class App {
 	private List<Transaction> transactionList;
 	private List<Account> accountList;
 	private List<Category> incomeCategoryList, expenseCategoryList;
-	private AccountService accountService;
-	private CategoryService categoryService;
-	private TransactionService transactionService;
-	private boolean monthView;
+	private final AccountService accountService;
+	private final CategoryService categoryService;
+	private final TransactionService transactionService;
 	private int month, year;
 	
 	private App () {
 		accountService = new AccountService();
 		categoryService = new CategoryService();
 		transactionService = new TransactionService();
-		
-		monthView = true;
 		month = LocalDateTime.now().getMonthValue();
 		year = LocalDateTime.now().getYear();
 		
@@ -49,37 +46,15 @@ public class App {
 	}
 	
 	public void loadTransactionData () {
-		if (isMonthView())
-			transactionList = transactionService.getTransactionsByMonth(month, year);
-		else 
-			transactionList = transactionService.getTransactionsByYear(year);
-	}
-	
-	/*
-	 * PUBLIC METHODS
-	*/
-	
-	public boolean isMonthView () {
-		return monthView;
+		transactionList = transactionService.getTransactionsByMonth(month, year);
 	}
 	
 	public int getMonth() {
 		return month;
 	}
 
-	public void setMonth(int month, int year) {
-		this.month = month;
-		this.year = year;
-		loadTransactionData();
-	}
-
 	public int getYear() {
 		return year;
-	}
-
-	public void setYear(int year) {
-		this.year = year;
-		loadTransactionData();
 	}
 
 	public List<Transaction> getTransactionList() {
@@ -97,53 +72,31 @@ public class App {
 	public List<Category> getExpenseCategoryList() {
 		return expenseCategoryList;
 	}
-
-	public void setMonthView(boolean monthView) {
-		this.monthView = monthView;
-		if (!monthView) {
-			month = 12;
-		}
-		loadTransactionData();
-	}
 	
 	public boolean latestTime() {
         LocalDateTime now = LocalDateTime.now();
-        if (isMonthView()) {
-            return year == now.getYear() && month == now.getMonthValue();
-        } else {
-            return year == now.getYear();
-        }
+        return year == now.getYear() && month == now.getMonthValue();
     }
 
     public void prevTimeStamp() {
-        if (isMonthView()) {
-            if (month == 1) { // If current month is January
-                month = 12; // Set month to December
-                year--; // Decrease the year
-            } else {
-                month--; // Just decrease the month
-            }
-        } else {
-            year--; // Decrease the year in year view
-        }
-        loadTransactionData(); // Reload transaction data
+        if (month == 1) {
+			month = 12;
+			year--;
+		} else {
+			month--;
+		}
+        loadTransactionData();
     }
 
     public void nextTimeStamp() {
         LocalDateTime now = LocalDateTime.now();
-        if (isMonthView()) {
-            if (month == 12) { // If current month is December
-                month = 1; // Set month to January
-                year++; // Increase the year
-            } else {
-                month++; // Just increase the month
-            }
-        } else {
-            if (year < now.getYear()) {
-                year++; // Increase the year in year view
-            }
-        }
-        loadTransactionData(); // Reload transaction data
+		if (month == 12) {
+			month = 1;
+			year++;
+		} else {
+			month++;
+		}
+        loadTransactionData();
     }
 
 	public static App getInstance () {
